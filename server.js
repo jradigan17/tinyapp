1a18f68ef30d848181ba9f8767d4dd310b56597f
jradigan17 <jradigan@hotmail.com>
2022-08-15 19:12:33 -0400

    user profile edits multiple accounts

--- server.js
+++ server.js
@@ -98,6 +98,43 @@ app.get("/login", (req, res) => {
   res.render("urls_login", templateVars);
 });
 
+app.get("/failedlogin", (req, res) => {
+  const user = findKeyByValue(userDatabase, req.cookies.username)
+  console.log(user)
+    const templateVars = {user: userDatabase[req.cookies.userID], username: userDatabase[user]}
+    res.clearCookie('username')
+    res.render("urls_loginfail", templateVars);
+});
+
+app.get("/failedregister", (req, res) => {
+  const tmpuser = {
+    firstname: req.cookies.firstname, 
+    lastname: req.cookies.lastname, 
+    username: req.cookies.username, 
+    password: req.cookies.password, 
+    email: req.cookies.email, 
+    address1: req.cookies.address1, 
+    address2: req.cookies.address2, 
+    city: req.cookies.city, 
+    province: req.cookies.province, 
+    postalcode: req.cookies.postalcode}
+  const templateVars = {user: userDatabase[req.cookies.userID], tmpuser, newuseremail: req.cookies.newuseremail, newusername: req.cookies.newusername}
+    res.clearCookie('username')
+    res.clearCookie('firstname') 
+    res.clearCookie('lastname')
+    res.clearCookie('username') 
+    res.clearCookie('password') 
+    res.clearCookie('email')
+    res.clearCookie('address1') 
+    res.clearCookie('address2') 
+    res.clearCookie('city')
+    res.clearCookie('province') 
+    res.clearCookie('postalcode') 
+    res.clearCookie('newuseremail')
+    res.clearCookie('newusername')
+    res.render("urls_registerfail", templateVars);
+});
+
 app.get("/logout", (req, res) => {
   res.clearCookie('userID')
   // console.log(req.cookies)
@@ -118,6 +155,13 @@ app.get("/profile/edit", (req, res) => {
   const templateVars = {user: userDatabase[req.cookies.userID]}
   res.render("urls_editprofile", templateVars);
 });
+
+app.get("/profile/err", (req, res) => {
+  const templateVars = {user: userDatabase[req.cookies.userID], newuseremail: req.cookies.newuseremail, newusername: req.cookies.newusername}
+  res.clearCookie('newuseremail')
+  res.clearCookie('newusername')
+  res.render("urls_editprofileerr", templateVars);
+});
 //----------------------------------------------------------
 
 //----------------------------------------------------------
@@ -131,7 +175,7 @@ app.listen(PORT, () => {
 // Post Actions
 app.post("/urls/new", (req, res) => {
   let longURL = req.body.longURL.includes("http:") || req.body.longURL.includes("http:") ? req.body.longURL : 'http://' + req.body.longURL;
-  let id = generateRandomString();
+  const id = generateRandomString();
   urlDatabase[id] = longURL;
   // console.log(urlDatabase);
   res.redirect(`/urls/${id}`);
@@ -146,11 +190,18 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/submitlogin", (req, res) => {
-  let user = findKeyByValue(userDatabase, req.body.username)
+  const user = findKeyByValue(userDatabase, req.body.username)
+  console.log("submitlogin", user)
+  if (!user) {
+    return res.redirect(`/failedlogin`)
+  } else if (req.body.password !== userDatabase[user].password) {
+    res.cookie("username", userDatabase[user].username)
+    console.log(userDatabase[user])
+    return res.redirect(`/failedlogin`)
+  } else {
   res.cookie("userID", user)
-  // res.cookie('username', req.body.username)
-  // console.log(req.cookies)
   res.redirect(`/urls`);
+  }
 });
 
 app.post("/logout", (req, res) => {
@@ -191,17 +242,83 @@ app.post("/urls/:id/update", (req, res) => {
 });
 
 app.post("/submitregister", (req, res) => {
-  
-  let userID = generateRandomString()
-  Object.assign(userDatabase, {[userID]: {userID: userID, firstname: req.body.firstname, lastname: req.body.lastname, username: req.body.username, password: req.body.password, email: req.body.email, address1: req.body.address1, address2: req.body.address2, city: req.body.city, province: req.body.province, postalcode: req.body.postalcode}})
-  res.cookie("userID", userID)
-  // res.cookie('username', req.body.username)
-  console.log(userDatabase)
-  res.redirect(`/urls`);
+  const newusername = findKeyByValue(userDatabase, req.body.username)
+  const newuseremail =  findKeyByValueEmail(userDatabase, req.body.email)
+
+  console.log(newusername, newuseremail)
+  if (!newusername && !newuseremail) {
+    const userID = generateRandomString()
+    Object.assign(userDatabase, {[userID]: {userID: userID, firstname: req.body.firstname, lastname: req.body.lastname, username: req.body.username, password: req.body.password, email: req.body.email, address1: req.body.address1, address2: req.body.address2, city: req.body.city, province: req.body.province, postalcode: req.body.postalcode}})
+    res.cookie("userID", userID)
+    res.redirect(`/urls`);
+  } else if (newuseremail) {
+    res.cookie("firstname", req.body.firstname)
+    res.cookie("lastname", req.body.lastname)
+    res.cookie("username", req.body.username)
+    res.cookie("password", req.body.password)
+    res.cookie("email", req.body.email)
+    res.cookie("address1", req.body.address1) 
+    res.cookie("address2", req.body.address2) 
+    res.cookie("city", req.body.city)
+    res.cookie("province", req.body.province)
+    res.cookie("postalcode", req.body.postalcode)
+    res.cookie("newuseremail", newuseremail)
+    return res.redirect(`/failedregister`)
+  } else if (newusername) {
+    res.cookie("firstname", req.body.firstname)
+    res.cookie("lastname", req.body.lastname)
+    res.cookie("username", req.body.username)
+    res.cookie("password", req.body.password)
+    res.cookie("email", req.body.email)
+    res.cookie("address1", req.body.address1) 
+    res.cookie("address2", req.body.address2) 
+    res.cookie("city", req.body.city)
+    res.cookie("province", req.body.province)
+    res.cookie("postalcode", req.body.postalcode)
+    res.cookie("newusername", newusername)
+    return res.redirect(`/failedregister`)
+  } 
 });
 
 app.post("/editprofile", (req, res) => {
   res.redirect(`/profile/edit`);
+});
+
+app.post("/profileupdate", (req, res) => {
+  const email = findKeyByValueNumE(userDatabase, req.body.email);
+  const username = findKeyByValueNumU(userDatabase, req.body.username);
+  console.log(email);
+  console.log(username);
+  if ((email.length === 0 || (email.length === 1 && email[0] === req.cookies.userID)) && (username.length === 0 || (username.length === 1 && username[0] === req.cookies.userID))) {
+    Object.assign(userDatabase[req.cookies.userID], {firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email, username: req.body.username, address1: req.body.address1, address2: req.body.address2, city: req.body.city, province: req.body.province, postalcode: req.body.postalcode})
+    console.log(userDatabase)
+    res.redirect(`/profile`);
+    
+  } else if ((username.length > 1 || username[0] !== req.cookies.userID) && (email.length > 1 || email[0] !== req.cookies.userID)) {
+    Object.assign(userDatabase[req.cookies.userID], {firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email, address1: req.body.address1, address2: req.body.address2, city: req.body.city, province: req.body.province, postalcode: req.body.postalcode})
+    res.cookie("newusername", req.body.username)
+    res.cookie("newuseremail", req.body.email)
+    res.redirect(`/profile/err`);
+
+  } else if (email.length > 1 || email[0] !== req.cookies.userID) {
+    Object.assign(userDatabase[req.cookies.userID], {firstname: req.body.firstname, lastname: req.body.lastname, username: req.body.username, address1: req.body.address1, address2: req.body.address2, city: req.body.city, province: req.body.province, postalcode: req.body.postalcode})
+    res.cookie("newuseremail", req.body.email)
+    res.redirect(`/profile/err`);
+
+  } else if (username.length > 1 || username[0] !== req.cookies.userID) {
+    Object.assign(userDatabase[req.cookies.userID], {firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email, address1: req.body.address1, address2: req.body.address2, city: req.body.city, province: req.body.province, postalcode: req.body.postalcode})
+    res.cookie("newusername", req.body.username)
+    res.redirect(`/profile/err`);
+  }
+});
+
+
+app.post("/deleteprofile", (req, res) => {
+  console.log("info", req.body)
+  delete userDatabase[req.cookies.userID]
+  console.log(userDatabase);
+  res.clearCookie("userID")
+  res.redirect(`/urls`);
 });
 
 app.post("/userprofile", (req, res) => {
@@ -230,6 +347,8 @@ const generateRandomString = () => {
 };
 //----------------------------------------------------------
 
+//----------------------------------------------------------
+// Object Key Search
 const findKeyByValue = function(object, value) {
   for (let item in object) {
     if (value === object[item].username) {
@@ -237,3 +356,32 @@ const findKeyByValue = function(object, value) {
     }
   }
 };
+
+const findKeyByValueEmail = function(object, value) {
+  for (let item in object) {
+    if (value === object[item].email) {
+      return item;
+    }
+  }
+};
+
+const findKeyByValueNumE = function(object, value) {
+  let array = [];
+  for (let item in object) {
+    if (value === object[item].email) {
+      array.push(item);
+    }
+  }
+  return array;
+};
+
+const findKeyByValueNumU = function(object, value) {
+  let array = [];
+  for (let item in object) {
+    if (value === object[item].username) {
+      array.push(item);
+    }
+  }
+  return array;
+};
+//----------------------------------------------------------
